2e1137d08631e1669e9aae238489866711d7187a
Alex Bain <alex@alexba.in>
2014-05-25 19:35:15 -0700

    Update provision-collectd.sh

--- provision-collectd.sh
+++ provision-collectd.sh
@@ -1,3 +1,8 @@
+# collectd is used to gather system load, memory utilization, disk space, network traffic, and more
 apt-get install -y collectd
+
+# use the config file included by this repository
 cp collectd/collectd.conf /etc/collectd/collectd.conf
+
+# restart with new config file
 service collectd restart

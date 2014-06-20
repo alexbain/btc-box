df20a2fd99f46fbab1c0295ae34a7ca6b5234011
Alex Bain <alex@alexba.in>
2014-06-20 19:54:44 -0400

    Use bitcoind data volume, don't expost ES ports publicly, ufw eth1 btc port expose

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -4,4 +4,4 @@ mkdir elasticsearch/data
 docker build -t elasticsearch elasticsearch/
 docker stop elasticsearch
 docker rm elasticsearch
-docker run -d -p 9200:9200 -p 9300:9300 -v $DIR/elasticsearch/data:/data/ --name elasticsearch elasticsearch /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
+docker run -d -v $DIR/elasticsearch/data:/data/ --name elasticsearch elasticsearch /elasticsearch/bin/elasticsearch -Des.config=/data/elasticsearch.yml
--- provision-ufw.sh
+++ provision-ufw.sh
@@ -1,17 +1,9 @@
 # reset everything
-ufw reset
+ufw reset --force
 
-# lock everything down on eth0
-ufw default deny incoming on eth0
-ufw default deny outgoing on eth0
-
-# lock everything down on eth1
-# ufw default deny incoming on eth1
-# ufw default deny outgoing on eth1
-
-# docker0 is internal and should be unrestricted
-ufw default allow incoming on docker0
-ufw default allow outgoing on docker0
+# lock everything down 
+ufw default deny incoming
+ufw default allow outgoing
 
 # Allow SSH in/out on eth0, limit people from brutce forcing
 ufw allow in on eth0 to any port 22 proto tcp
@@ -28,11 +20,13 @@ ufw allow out on eth0 to any port 80 proto tcp
 
 # Expose bitcoind communication port
 ufw allow in on eth0 to any port 8333 proto tcp
+ufw allow in on eth1 to any port 8333 proto tcp
 ufw allow out on eth0 to any port 8333 proto tcp
+ufw allow out on eth1 to any port 8333 proto tcp
 
-# Expose elasticsearch (needed by Kibana)
-# ufw allow in on eth0 to any port 9200 proto tcp
-# ufw allow out on eth0 to any port 9200 proto tcp
+# Expose bitcoind json-rpc port via eth1
+ufw allow in on eth1 to any port 8332 proto tcp
+ufw allow out on eth1 to any port 8332 proto tcp
 
 # Enable all ufw rules
 ufw enable
